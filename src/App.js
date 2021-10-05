3d5755e2cc88c72e195cb0add45b906442de3654
RobFenger <robfenger@gmail.com>
2021-10-05 12:34:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ocean from './images/ocean.jpg';
+
+
+const title = '';
+
+const background = <img className="background" alt="ocean" src={ocean}/>
+
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key= {animal}
+      className= 'animal'
+      alt= {animal}
+      src= {animals[animal].image}
+      aria-label= {animal}
+      role= 'button'
+      onClick= {displayFact}
+    />
+  )
+}
+
+function displayFact (e) {
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const randomIndex = Math.floor(Math.random() * animalInfo.facts.length);
+  const funFact = animalInfo.facts[randomIndex];
+  document.getElementById('fact').innerHTML = funFact;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+<h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+{background}
+<p id="fact"></p>
+<div className="animals">{images}</div>
+</div>
   );
 }
+
+
+
+
+ReactDOM.render(App, document.getElementById("root"));
 
 export default App;
